7f793cc7db87cdecd988d4fdcb3aecf78790d4c2
djumanov <diyorbekjumanov1120@gmail.com>
2023-04-06 15:02:44 +0500

    de-serializer

--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,6 @@ from rest_framework import serializers
 
 class TaskSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=64)
-    description = serializers.TextField()
-    completed   = serializers.BooleanField(default=False, blank=True)
+    description = serializers.CharField()
+    completed   = serializers.BooleanField(default=False)
 
--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+# import models
+from .models import Task
+
+# import seralizers
+from .serializers import TaskSerializer
+
+
+@api_view(['GET'])
+def get_tasks(request: Request) -> Response:
+    '''get all tasks'''
+    # all tasks
+    tasks = Task.objects.all()
+    # task_list
+    tasks_list = []
+    for task in tasks:
+        serializer = TaskSerializer(task)
+        tasks_list.append(serializer.data)
+    
+    return Response(tasks_list)
